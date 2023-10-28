37f89bad1a49bb476932b31b5d119a53df174aaf
Jaime <garciadiazjaime@gmail.com>
2023-10-28 11:34:09 -0500

    feat: gtag

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+
+import TagManager from "react-gtm-module";
+
+import { useEffect } from "react";
+
+const tagManagerArgs = {
+  gtmId: "GTM-5TDDZW8S",
+};
+
 export default function Home() {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <main>
       <h1>Live Music</h1>
