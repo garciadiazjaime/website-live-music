3d5db34a28f55da0a5c5adf4ed7bdba469c5d6f5
Jaime <garciadiazjaime@gmail.com>
2023-11-08 19:20:40 -0600

    feat: print gpt command

--- support/gpt.js
+++ support/gpt.js
@@ -1,35 +1,48 @@
-const events = require('../public/events.json');
-const axios = require('axios');
+const fetch = require("node-fetch");
 
-async function gptRequest() {
-  const titles = events.map(item => item.title).slice(0,2);
-  const prompt = `Write the text for an add inviting people from chicago to the following events, make it fun and snappy. Aim for 30 seconds: ${titles.join(', ')}`
-  console.log(prompt)
+const events = require("../public/events.json");
 
-  const response = await axios.post(
-    'https://api.openai.com/v1/chat/completions',
-    {
-      "model": "gpt-3.5-turbo",
-      "messages": [{"role": "user", "content": prompt}],
-      "temperature": 0.7,
-      "max_tokens": 100,
+const TOP_EVENTS_LIMIT = 2;
+
+async function main() {
+  console.log("starting...");
+
+  const titles = events.map((item) => item.name).slice(0, TOP_EVENTS_LIMIT);
+  const prompt = `Write the text for an ad inviting people from chicago to the following events, make it fun and snappy. Aim for 30 seconds: \n${titles.join(
+    "\n"
+  )}`;
+  console.log(prompt);
+
+  if (!process.env.OPEN_AI_API_KEY) {
+    console.log("GTP Token not defined");
+    return;
+  }
+
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "post",
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.7,
+      max_tokens: 100,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPEN_AI_API_KEY}`,
     },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-      },
-    }
-  );
-  // Handle the API response here
-  const { data } = response;
-  const videoScript = `Create a video using exactly this script\n
+  });
 
-  ${data.choices[0].message.content}\n
+  const { data } = await response.json();
+  const gptResponse = data.choices[0].message.content;
+
+  const videoScript = `Create a video using exactly this script:\n
+
+  ${gptResponse}\n
 
   Make the background music exciting and fast paced\n
 
-  Settings: Use a female voice with a midwestern accent.`
+  Settings: Use a female voice with a midwestern accent.`;
   console.log(videoScript);
 }
-gptRequest().then(() => console.log('END'));
+
+main().then(() => console.log("END"));
