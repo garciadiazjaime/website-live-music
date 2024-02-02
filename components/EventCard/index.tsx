5c2080c62bcb1ed3f3191e341156699a877034d4
alex-romo <alex@nebulosastudio.com>
2024-02-01 19:43:54 -0800

    feat: scrollin is fixed, pin button, and some restyling

--- components/EventCard/index.tsx
+++ components/EventCard/index.tsx
@@ -7,9 +7,10 @@ interface Props {
   event: Event;
   index: number;
   selected: boolean | undefined;
+  setPin: () => void;
 }
 
-const EventCard = ({ event, index, selected = false }: Props) => {
+const EventCard = ({ event, index, selected = false, setPin }: Props) => {
   const getImage = (event: Event): string => {
     const artist = event.artists?.find((artist) => artist.metadata?.image);
     if (artist) {
@@ -23,24 +24,31 @@ const EventCard = ({ event, index, selected = false }: Props) => {
     return event.image;
   };
 
-  const eventHandler = (event: Event) => window.open(event.url, "_blank");
+  const gotoEventPage = (event: Event) => window.open(event.url, "_blank");
+  console.log(event)
 
-  return <div className="flex flex-col w-full h-full items-end">
+  return <div className="flex flex-col w-full h-full items-end group">
     <Image
       priority={index === 0}
       src={getImage(event)}
       width={300}
       height={150}
       alt={event.name}
-      className="object-cover w-2/3 h-32 -mb-24 mr-8 z-10 bg-white"
+      className="object-cover w-3/4 h-32 -mb-24 mr-4 z-10 bg-white"
     />
-    <div className={`flex flex-col w-full items-start grow ${selected ? 'bg-fuchsia-800/30 shadow-xl' : 'bg-gray-950/40'} pt-2 pb-6 text-white relative `}>
-      <div className="absolute -top-3 right-2 z-30 w-auto">
-        <Link href="/share" className="bg-fuchsia-400 p-1.5 mb-2 flex">
+    <div className={`flex flex-col w-full items-start grow ${selected ? 'bg-fuchsia-800/50 shadow-xl' : 'bg-gray-950/40 group-hover:bg-fuchsia-800/30'} py-2 mr-2 text-white relative `}>
+      <div className={`absolute bottom-2 -right-2 z-30 w-auto gap-2 flex flex-col ${!selected && 'lg:opacity-0 group-hover:opacity-100 transition-all duration-300'}`}>
+        <button onClick={setPin} className={`bg-fuchsia-400 p-2 flex ${selected && 'hidden'}`}>
+          <Image src="/images/pin.svg" width="68" height="54" className="w-5 h-auto" alt=""/>
+        </button>
+        <button className="bg-fuchsia-400 p-2 flex">
           <Image src="/images/share-btn.svg" width="68" height="54" className="w-5 h-auto" alt=""/>
-        </Link>
-        <button className="bg-fuchsia-400 p-1.5 flex">
+        </button>
+        <button className="bg-fuchsia-400 p-2 flex">
           <Image src="/images/show-location.svg" width="68" height="54" className="w-5 h-auto" alt=""/>
+        </button>
+        <button onClick={() => gotoEventPage(event)} className="bg-fuchsia-400 p-2 flex">
+          <Image src="/images/tickets.svg" width="68" height="54" className="w-5 h-auto" alt=""/>
         </button>
       </div>
       <h3 className="font-bold text-fuchsia-400 text-3xl pb-2 pl-3 w-auto">
@@ -53,13 +61,10 @@ const EventCard = ({ event, index, selected = false }: Props) => {
             `}<span className="text-base">PM</span>
       </h3>
       <h3 className="z-30 bg-fuchsia-400 text-base mb-10 px-3 italic">{event.location.name}</h3>
-      <button onClick={() => eventHandler(event)} className="text-left font-bold text-elipsis text-4xl px-6 flex justify-between items-center w-full gap-4">
-        <h2 className="capitalize  text-left text-2xl">
-          {event.name.toLowerCase()}
-        </h2>
-        <Image src="/images/chevron.svg" width="22" height="56" className="w-4 h-auto" alt=""/>
-      </button>
-      <div className="flex pl-2">
+      <h2 className="capitalize  text-left text-2xl font-bold text-elipsis px-6 w-full">
+        {event.name.toLowerCase()}
+      </h2>
+      <div className={`flex pl-2 ${!selected && 'lg:opacity-0 group-hover:opacity-100 transition-all duration-300'}`}>
         <SocialLinks event={event} />
       </div>
     </div>
