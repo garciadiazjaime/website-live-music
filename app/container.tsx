77be2f10b926401935067add3fb5723b759cd5e8
Jaime <garciadiazjaime@gmail.com>
2024-04-11 19:03:06 -0500

    test: revert changes

--- app/container.tsx
+++ app/container.tsx
@@ -1,9 +1,58 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import { InfoBtn, Logo, LogoText } from "@/components/svgs";
 import { tokens } from "@/support/token";
+import DayPicker from "@/components/DayPicker";
 import { Event } from "@/support/types";
 import EventCard from "@/components/EventCard/v2";
 
+export async function getEventsByDay() {
+  const url = "/.netlify/functions/events";
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    return;
+  }
+
+  const data = await res.json();
+
+  return data.reduce((eventsByDay: Record<string, Event[]>, event: Event) => {
+    const day = new Date(event.start_date).toJSON().split("T")[0];
+
+    if (!eventsByDay[day]) {
+      eventsByDay[day] = [];
+    }
+
+    eventsByDay[day].push(event);
+
+    return eventsByDay;
+  }, {});
+}
+
 export default function Home({ events }: { events: Event[] }) {
+  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedEvents, setSelectedEvents] = useState<Event[]>(events);
+  const [eventsByDay, setEventsByDay] = useState<Record<string, Event[]>>({});
+
+  const fetchEvents = async () => {
+    const data = await getEventsByDay();
+    setEventsByDay(data);
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  useEffect(() => {
+    if (!selectedDate || !eventsByDay[selectedDate]) {
+      return;
+    }
+
+    setSelectedEvents(eventsByDay[selectedDate]);
+  }, [selectedDate, eventsByDay]);
+
   return (
     <main
       style={{
@@ -57,7 +106,10 @@ export default function Home({ events }: { events: Event[] }) {
         >
           <Logo />
         </div>
-
+        <DayPicker
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+        />
         <div
           style={{
             width: "30px",
@@ -68,6 +120,40 @@ export default function Home({ events }: { events: Event[] }) {
           <InfoBtn />
         </div>
       </nav>
+      <section
+        style={{
+          position: "sticky",
+          top: 80,
+        }}
+      >
+        {selectedEvents.map((event) => (
+          <EventCard key={event.slug} event={event} />
+        ))}
+      </section>
+
+      <style jsx>{`
+        .brand {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20%;
+          gap: 2rem;
+          width: 60vw;
+
+          @media (min-width: ${tokens.breakpoints.md}) {
+            width: 40vw;
+          }
+
+          @media (min-width: ${tokens.breakpoints.lg}) {
+            width: 25vw;
+          }
+
+          svg:first-child {
+            width: 40%;
+            margin-left: -15%;
+          }
+        }
+      `}</style>
     </main>
   );
 }
